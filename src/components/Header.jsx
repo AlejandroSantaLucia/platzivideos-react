b3d36ed219e4cd43ade97eac382147aa6ca21550
Alejandro <alejandrosantalucia@gmail.com>
2021-03-12 15:01:47 -0300

    variables y media query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 
 import React from 'react'
 import '../assets/styles/components/Header.scss'
+import Logo from '../assets/statics/logo-platzi-video-BW2.png'
+import UserIcos from '../assets/statics/user-icon.png'
 
 const Header = () =>{
     return(
 
         <header className="header">
-        <img className="header__img" src="../assets/logo-platzi-video-BW2.png" alt="Platzi Video"/>
+        <img className="header__img" src={Logo} alt="Platzi Video"/>
         <div className="header__menu">
         <div className="header__menu--profile">
-            <img src="../assets/user-icon.png" alt=""/>
+            <img src={UserIcos} alt=""/>
             <p>Perfil</p>
         </div>
         <ul>
